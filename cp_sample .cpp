7952226cc8dd17da79ca311f90a3899990efa16b
Mahfuj_SUST <107198635+mahfujalam77452@users.noreply.github.com>
2024-02-16 18:30:48 +0600

    Create cp_sample .cpp

---
+++ cp_sample .cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> order_set; // find_by_order, order_of_key
+template <typename T>
+using order_multiset = __gnu_pbds::tree<T, __gnu_pbds::null_type, std::less_equal<T>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;;
+//kthElement = *myTree.find_by_order(1), index(zero based) = myTree.order_of_key(8);
+//order_multiset<int>st;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define int long long
+const int N = 2e5 + 10;
+const int M=1e9+7;
+
+
+int32_t main()
+{
+   
+     IOS
+     int t;
+     cin>>t;
+     while(t--){
+         
+         
+        
+     }
+	return 0;
+}
+
