ead50ebdc79043925d0b2a2cf9bc8cc09017c3ba
Mahfuj_SUST <107198635+mahfujalam77452@users.noreply.github.com>
2024-10-27 13:04:33 +0600

    Create Number of element greater then a.cpp

---
+++ Number of element greater then a.cpp
@@ -0,0 +1,3 @@
+// Count the number of elements in right_elements that are greater than arr[i],here right_elements
+        // is a multiset
+       distance(right_elements.upper_bound(arr[i]), right_elements.end());
